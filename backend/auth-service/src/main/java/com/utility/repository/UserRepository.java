9d089795a1424a08fac81bf687dd0f66516b4ecb
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:48:23 +0530

    Added auth repository

--- backend/auth-service/src/main/java/com/utility/repository/UserRepository.java
+++ backend/auth-service/src/main/java/com/utility/repository/UserRepository.java
@@ -10,5 +10,7 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Mono<User> findByUsername(String username);
+    Mono<Boolean> existsByUsername(String username);
+    Mono<Boolean> existsByEmail(String email);
 }
 

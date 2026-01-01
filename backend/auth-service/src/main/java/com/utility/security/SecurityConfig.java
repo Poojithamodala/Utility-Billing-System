3f71df345145532bb789e2f8af391096cb56ddd1
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:50:03 +0530

    Added security config

--- backend/auth-service/src/main/java/com/utility/security/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/utility/security/SecurityConfig.java
@@ -2,28 +2,38 @@ package com.utility.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-@EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/login").permitAll()
-                        .pathMatchers("/auth/register").hasRole("ADMIN")
+                .csrf(csrf -> csrf.disable()) // correct WebFlux syntax
+                .authorizeExchange(exchange -> exchange
+                		.pathMatchers(HttpMethod.POST, "/auth/internal/**").hasRole("ADMIN")
+                		.pathMatchers(HttpMethod.POST, "/auth/activate").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
                         .anyExchange().authenticated()
                 )
-
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

4546a341bce886d78ba6c8a0b4cbdb7ff6b80e1d
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:47:36 +0530

    Added user model

--- backend/auth-service/src/main/java/com/utility/model/User.java
+++ backend/auth-service/src/main/java/com/utility/model/User.java
@@ -4,22 +4,24 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Document(collection = "users")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
     @Id
     private String id;
-
-    private String userId;
     private String username;
     private String email;
     private String password;
-    private String role;
-    private boolean enabled = true;
+    private Role role;
+    
+    private boolean enabled;  
+    private int failedAttempts = 0;
 }

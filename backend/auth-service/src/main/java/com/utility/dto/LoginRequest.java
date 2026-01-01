fb6a9656ecc9e8a40d4a676b7d414f2da16ece44
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:46:15 +0530

    Added dto for login request

--- backend/auth-service/src/main/java/com/utility/dto/LoginRequest.java
+++ backend/auth-service/src/main/java/com/utility/dto/LoginRequest.java
@@ -1,9 +1,17 @@
 package com.utility.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+	
+	@NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be 3–20 characters")
     private String username;
+	
+	@NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }

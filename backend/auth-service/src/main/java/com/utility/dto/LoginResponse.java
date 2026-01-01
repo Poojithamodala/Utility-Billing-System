c144d58682a1bb814f1e1faba592506ac7c11819
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:46:44 +0530

    Added dto for login response

--- backend/auth-service/src/main/java/com/utility/dto/LoginResponse.java
+++ backend/auth-service/src/main/java/com/utility/dto/LoginResponse.java
@@ -1,12 +1,17 @@
 package com.utility.dto;
 
+import com.utility.model.Role;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
     private String token;
     private String role;
-    private String username;
 }

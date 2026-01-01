c95be445737b2a392f2e5c9045dd7ce884503dc1
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:47:12 +0530

    Added dto for register request

--- backend/auth-service/src/main/java/com/utility/dto/RegisterRequest.java
+++ backend/auth-service/src/main/java/com/utility/dto/RegisterRequest.java
@@ -1,11 +1,37 @@
 package com.utility.dto;
 
+import com.utility.model.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
+	
+	@NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be 3-20 characters")
     private String username;
+	
+	@NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
+	
+	@NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+	@Pattern(
+			  regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+			  message = "Password must contain uppercase, lowercase, number and special character"
+			)
     private String password;
-    private String role;
+	
+	@NotNull(message = "Role is required")
+    private Role role;
 }

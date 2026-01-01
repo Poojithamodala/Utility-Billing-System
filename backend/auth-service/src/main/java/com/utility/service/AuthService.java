96379e58bf71dca6cea5e4f302a666391bb80acd
Poojithamodala <poojithamodala26@gmail.com>
2026-01-01 13:50:35 +0530

    Added auth service

--- backend/auth-service/src/main/java/com/utility/service/AuthService.java
+++ backend/auth-service/src/main/java/com/utility/service/AuthService.java
@@ -1,13 +1,16 @@
 package com.utility.service;
 
+import com.utility.dto.ActivateRequest;
+import com.utility.dto.ConsumerAuthCreateRequest;
 import com.utility.dto.LoginRequest;
 import com.utility.dto.LoginResponse;
 import com.utility.dto.RegisterRequest;
-import com.utility.model.User;
 
 import reactor.core.publisher.Mono;
 
 public interface AuthService {
+    Mono<Void> register(RegisterRequest request);
     Mono<LoginResponse> login(LoginRequest request);
-    Mono<User> register(RegisterRequest request);
+    Mono<Void> createConsumerUser(ConsumerAuthCreateRequest request);
+    Mono<Void> activateConsumer(ActivateRequest request);
 }

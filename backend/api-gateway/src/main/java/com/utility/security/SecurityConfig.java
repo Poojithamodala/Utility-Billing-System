de875b79b07ed28be96926ad18c2d0998f7090e1
Poojithamodala <poojithamodala26@gmail.com>
2026-01-02 20:06:23 +0530

    Added api gateway

---
+++ backend/api-gateway/src/main/java/com/utility/security/SecurityConfig.java
@@ -0,0 +1,109 @@
+package com.utility.security;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Flux;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+            .authorizeExchange(ex -> ex
+                .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                // Auth Service
+                .pathMatchers("/auth-service/auth/internal/**").hasRole("ADMIN")
+                .pathMatchers("/auth-service/auth/**").permitAll()
+                
+
+                // Consumer Service
+                .pathMatchers("/consumer-service/consumers/profile").authenticated()
+                .pathMatchers(HttpMethod.POST, "/consumer-service/consumers").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.PUT, "/consumer-service/consumers/**").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.DELETE, "/consumer-service/consumers/**").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.GET, "/consumer-service/consumers/**").hasAnyRole("ADMIN", "CONSUMER")
+                
+                //connection service
+                .pathMatchers(HttpMethod.POST, "/connection-service/connections").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.GET, "/connection-service/connections").hasAnyRole("ADMIN", "BILLING_OFFICER")
+                .pathMatchers(HttpMethod.GET, "/connection-service/connections/consumer/**").hasAnyRole("ADMIN","CONSUMER")
+                .pathMatchers(HttpMethod.GET, "/connection-service/connections/**").hasAnyRole("ADMIN", "CONSUMER", "BILLING_OFFICER")
+                .pathMatchers(HttpMethod.POST, "/connection-service/tariffs").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.GET, "/connection-service/tariffs/utility/**").hasAnyRole("ADMIN", "BILLING_OFFICER", "CONSUMER")
+                .pathMatchers(HttpMethod.GET, "/connection-service/tariffs/**").hasAnyRole("ADMIN", "BILLING_OFFICER", "CONSUMER")
+                
+                //meter reading service
+                .pathMatchers(HttpMethod.POST, "/meter-reading-service/meter-readings").hasRole("BILLING_OFFICER")
+				.pathMatchers(HttpMethod.GET, "/meter-reading-service/meter-readings").hasRole("ADMIN")
+				.pathMatchers(HttpMethod.GET, "/meter-reading-service/meter-readings/connection/**").hasAnyRole("ADMIN", "BILLING_OFFICER", "CONSUMER")
+				.pathMatchers(HttpMethod.GET, "/meter-reading-service/meter-readings/*").hasAnyRole("ADMIN", "BILLING_OFFICER")
+				
+				//billing service
+				.pathMatchers(HttpMethod.POST, "/billing-service/bills/generate").hasAnyRole("ADMIN", "BILLING_OFFICER")
+				.pathMatchers(HttpMethod.GET, "/billing-service/bills/consumer/{consumerId}").hasAnyRole("ADMIN", "CONSUMER")
+
+                .anyExchange().authenticated()
+            )
+
+            .oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwt ->
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                )
+            )
+            .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(
+            @Value("${spring.security.jwt.secret}") String secret) {
+
+        SecretKey key = new SecretKeySpec(
+            secret.getBytes(StandardCharsets.UTF_8),
+            "HmacSHA256"
+        );
+
+        return NimbusReactiveJwtDecoder.withSecretKey(key).build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter authoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
+
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+
+        ReactiveJwtAuthenticationConverter converter =
+            new ReactiveJwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(
+            jwt -> Flux.fromIterable(authoritiesConverter.convert(jwt))
+        );
+
+        return converter;
+    }
+}

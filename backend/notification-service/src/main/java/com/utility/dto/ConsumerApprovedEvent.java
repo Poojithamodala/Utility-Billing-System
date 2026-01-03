b0771709f96c8a7911b06b84dcb04043606f2eaf
Poojithamodala <poojithamodala26@gmail.com>
2026-01-04 02:27:24 +0530

    added dto's for consumer registration approve and reject

---
+++ backend/notification-service/src/main/java/com/utility/dto/ConsumerApprovedEvent.java
@@ -0,0 +1,14 @@
+package com.utility.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConsumerApprovedEvent {
+    private String email;
+    private String name;
+}
+

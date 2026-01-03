49e3a88a4278a5fd4eae2bacf68db335ce4923d7
Poojithamodala <poojithamodala26@gmail.com>
2026-01-03 15:38:18 +0530

    Added kafka producer for mail when payment is done

---
+++ backend/payment-service/src/main/java/com/utility/dto/PaymentSuccessEvent.java
@@ -0,0 +1,16 @@
+package com.utility.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentSuccessEvent {
+    private String billId;
+    private String consumerEmail;
+    private double amountPaid;
+    private LocalDate paymentDate;
+}

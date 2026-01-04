9faf757e3f0a16db05a1ff99f74c86604942bcec
Poojithamodala <poojithamodala26@gmail.com>
2026-01-05 01:09:46 +0530

    added login component

--- frontend/utility-billing-frontend/src/app/components/login/login.ts
+++ frontend/utility-billing-frontend/src/app/components/login/login.ts
@@ -1,60 +1,75 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  credentials = {
+  loginData = {
     username: '',
     password: ''
   };
 
   loading = false;
-  error = '';
+  errorMessage = '';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef) { }
 
   login() {
-    this.error = '';
+    this.errorMessage = '';
     this.loading = true;
 
-    this.authService.login(this.credentials).subscribe({
-      next: (res) => {
+    this.http.post<any>(
+      'http://localhost:8765/auth-service/auth/login',
+      this.loginData
+    ).subscribe({
+      next: res => {
         localStorage.setItem('token', res.token);
         localStorage.setItem('role', res.role);
-
-        // Role-based redirect
         switch (res.role) {
           case 'ADMIN':
-            this.router.navigate(['/admin']);
+            this.router.navigate(['/admin/home']);
             break;
           case 'BILLING_OFFICER':
-            this.router.navigate(['/billing']);
+            this.router.navigate(['/billing/home']);
             break;
           case 'ACCOUNTS_OFFICER':
-            this.router.navigate(['/accounts']);
+            this.router.navigate(['/accounts/home']);
             break;
           case 'CONSUMER':
-            this.router.navigate(['/consumer']);
+            this.fetchConsumerProfile();
             break;
-          default:
-            this.router.navigate(['/login']);
         }
+        this.cdr.detectChanges();
       },
-      error: (err) => {
-        this.error = err.error?.message || 'Login failed';
+      error: err => {
+        this.errorMessage = err.error?.message || 'Invalid credentials';
         this.loading = false;
+        this.cdr.detectChanges();
       }
     });
   }
+
+  fetchConsumerProfile() {
+  this.http.get<any>(
+    'http://localhost:8765/consumer-service/consumers/profile'
+  ).subscribe({
+    next: consumer => {
+      localStorage.setItem('consumerId', consumer.id);
+      this.router.navigate(['/consumer/home']);
+      this.cdr.detectChanges();
+    },
+    error: () => {
+      this.errorMessage = 'Unable to load consumer profile';
+      this.loading = false;
+      this.cdr.detectChanges();
+    }
+  });
+}
 }

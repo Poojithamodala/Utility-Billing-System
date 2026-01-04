52394d4dc758dc8c569fea8e33757989336776fa
Poojithamodala <poojithamodala26@gmail.com>
2026-01-05 01:14:42 +0530

    added home component

---
+++ frontend/utility-billing-frontend/src/app/components/home/home.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  
+}

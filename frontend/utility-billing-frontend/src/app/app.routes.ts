eae26f330d716aefda32ffad57dceddbdb7bf468
Poojithamodala <poojithamodala26@gmail.com>
2026-01-05 01:13:06 +0530

    added route and providers

--- frontend/utility-billing-frontend/src/app/app.routes.ts
+++ frontend/utility-billing-frontend/src/app/app.routes.ts
@@ -1,19 +1,39 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 import { Login } from './components/login/login';
-import { ActivateAccount } from './components/activate-account/activate-account';
+import { Register } from './components/register/register';
 import { AppLayout } from './layout/app-layout/app-layout';
+import { AdminHome } from './components/admin/admin-home/admin-home';
+import { AdminRequests } from './components/admin/admin-requests/admin-requests';
+import { ApprovedConsumers } from './components/admin/approved-consumers/approved-consumers';
+import { ActivateAccount } from './components/activate-account/activate-account';
+import { ConsumerHome } from './components/consumer/consumer-home/consumer-home';
+import { ConsumerConnections } from './components/consumer/consumer-connections/consumer-connections';
+import { ConsumerProfile } from './components/consumer/consumer-profile/consumer-profile';
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'login', component: Login },
+    { path: 'register', component: Register },
     { path: 'activate', component: ActivateAccount },
     {
-        path: '',
+        path: 'admin',
         component: AppLayout,
         children: [
-            // { path: 'admin', loadComponent: () => import('./admin/admin.component') },
-            // { path: 'billing', loadComponent: () => import('./billing/billing.component') },
-            // { path: 'accounts', loadComponent: () => import('./accounts/accounts.component') },
-            // { path: 'consumer', loadComponent: () => import('./consumer/consumer.component') }
+            { path: 'home', component: AdminHome },
+            { path: 'requests', component: AdminRequests },
+            { path: 'consumers', component: ApprovedConsumers}
+        ]
+    },
+    {
+        path: 'consumer',
+        component: AppLayout,
+        children: [
+            { path: 'home', component: ConsumerHome },
+            { path: 'connections', component: ConsumerConnections},
+            { path: 'profile', component: ConsumerProfile }
+            // { path: 'bills', component: ConsumerBillsComponent }
         ]
     }
+
 ];

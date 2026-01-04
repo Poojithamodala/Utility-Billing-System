9752918c31632c78af9a0e9c622a27e420694fd2
Poojithamodala <poojithamodala26@gmail.com>
2026-01-05 01:10:21 +0530

    added a layout

--- frontend/utility-billing-frontend/src/app/layout/app-layout/app-layout.ts
+++ frontend/utility-billing-frontend/src/app/layout/app-layout/app-layout.ts
@@ -1,47 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
+import { Topbar } from '../topbar/topbar';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, RouterOutlet, Sidebar, Topbar],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.css',
 })
 export class AppLayout {
-  menu: { label: string; path: string }[] = [];
 
-  ngOnInit() {
-    const role = localStorage.getItem('role');
-
-    if (role === 'ADMIN') {
-      this.menu = [
-        { label: 'Consumers', path: 'consumers' },
-        { label: 'Officers', path: 'officers' },
-        { label: 'Tariffs', path: 'tariffs' }
-      ];
-    }
-
-    if (role === 'BILLING_OFFICER') {
-      this.menu = [
-        { label: 'Meter Readings', path: 'meter-readings' },
-        { label: 'Generate Bills', path: 'bills' }
-      ];
-    }
-
-    if (role === 'ACCOUNTS_OFFICER') {
-      this.menu = [
-        { label: 'Payments', path: 'payments' },
-        { label: 'Outstanding', path: 'outstanding' }
-      ];
-    }
-
-    if (role === 'CONSUMER') {
-      this.menu = [
-        { label: 'My Bills', path: 'bills' },
-        { label: 'Payments', path: 'payments' }
-      ];
-    }
-  }
 }

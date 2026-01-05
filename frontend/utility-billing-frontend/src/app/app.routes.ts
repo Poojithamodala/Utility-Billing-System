61e1af3d489da71670f027d1385fb3d1511e4837
Poojithamodala <poojithamodala26@gmail.com>
2026-01-05 21:50:09 +0530

    added routes

--- frontend/utility-billing-frontend/src/app/app.routes.ts
+++ frontend/utility-billing-frontend/src/app/app.routes.ts
@@ -12,6 +12,8 @@ import { ConsumerConnections } from './components/consumer/consumer-connections/
 import { ConsumerProfile } from './components/consumer/consumer-profile/consumer-profile';
 import { ConsumerTariffs } from './components/consumer/consumer-tariffs/consumer-tariffs';
 import { ConsumerBills } from './components/consumer/consumer-bills/consumer-bills';
+import { ConsumerPayments } from './components/consumer/consumer-payments/consumer-payments';
+import { ConnectionRequests } from './components/admin/connection-requests/connection-requests';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -24,7 +26,8 @@ export const routes: Routes = [
         children: [
             { path: 'home', component: AdminHome },
             { path: 'requests', component: AdminRequests },
-            { path: 'consumers', component: ApprovedConsumers}
+            { path: 'consumers', component: ApprovedConsumers},
+            { path: 'connection-requests', component: ConnectionRequests}
         ]
     },
     {
@@ -35,7 +38,8 @@ export const routes: Routes = [
             { path: 'connections', component: ConsumerConnections},
             { path: 'profile', component: ConsumerProfile },
             { path: 'tariffs', component: ConsumerTariffs },
-            { path: 'bills', component: ConsumerBills }
+            { path: 'bills', component: ConsumerBills },
+            { path: 'payments', component: ConsumerPayments }
         ]
     }
 

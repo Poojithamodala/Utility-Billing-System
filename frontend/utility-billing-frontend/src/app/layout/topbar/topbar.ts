2ce4330311c67b8dd30542d7656acba894b47242
Poojithamodala <poojithamodala26@gmail.com>
2026-01-09 21:23:42 +0530

    added an popup for logout button

--- frontend/utility-billing-frontend/src/app/layout/topbar/topbar.ts
+++ frontend/utility-billing-frontend/src/app/layout/topbar/topbar.ts
@@ -13,9 +13,30 @@ export class Topbar {
 
   constructor(private router: Router, private cdr: ChangeDetectorRef) {}
 
-  logout() {
-    localStorage.clear();
-    this.router.navigate(['/login']);
-    this.cdr.detectChanges();
-  }
+  showConfirm = false;
+
+logout() {
+  this.showConfirm = true;
+}
+
+confirmLogout() {
+  localStorage.clear();
+  this.router.navigate(['/login']);
+  this.showConfirm = false;
+  this.cdr.detectChanges();
+}
+
+cancelLogout() {
+  this.showConfirm = false;
+}
+  // logout() {
+  // const confirmLogout = window.confirm('Are you sure you want to logout?');
+
+  // if (!confirmLogout) {
+  //   return;
+  // }
+  //   localStorage.clear();
+  //   this.router.navigate(['/login']);
+  //   this.cdr.detectChanges();
+  // }
 }
